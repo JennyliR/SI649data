da9743ca4f80d0c69adb1ffa378fe527d38bece8
Ruijia Li <41889437+JennyliR@users.noreply.github.com>
2021-11-13 22:32:11 -0500

    Update jennyrli_redlake.py

--- jennyrli_redlake.py
+++ jennyrli_redlake.py
@@ -20,7 +20,7 @@ opacity = alt.condition(selection, alt.value(1), alt.value(0.5))
 selection = alt.selection_single(nearest=True, encodings=['y'], init={'y':'Income Per Capita'}) #selection for category
 opacity = alt.condition(selection, alt.value(1), alt.value(0.5))
 ##Bar Chart
-bar = alt.Chart(df, width=700, height=200).mark_bar(size=50).encode(
+bar = alt.Chart(df, width=1000, height=200).mark_bar(size=50).encode(
   alt.X('Rank:Q',title = None,axis=None),
   alt.Y('Category',title = None, axis=None,sort=alt.EncodingSortField(
         field='Rank', order='ascending'
@@ -44,7 +44,7 @@ highlight = alt.selection(type='single', on='mouseover',
                           fields=['Location'], nearest=False, empty='none')
 selection2 = alt.selection_interval(bind='scales', encodings=['y'])
 
-points = alt.Chart(df, width=500, height=150).mark_circle().encode(
+points = alt.Chart(df, width=1000, height=250).mark_circle().encode(
     alt.X('Year:O',title = None),
     alt.Y('Value:Q',title = None, axis=None),
     opacity=alt.value(0),
@@ -52,18 +52,18 @@ points = alt.Chart(df, width=500, height=150).mark_circle().encode(
     color = alt.condition(alt.datum.Location == 'Red Lake, MN', 'Location:N', alt.value("#997F6A"), legend=None),
     tooltip=['Location','Value', 'Unit', 'Year'],
 ).transform_filter(
-    selection).transform_filter(alt.datum.Location != 'Minnesota').properties(width=600,height= 400) #Changed
+    selection).transform_filter(alt.datum.Location != 'Minnesota').properties(width=1100, height= 400) #Changed
 
-line = alt.Chart(df, width=500, height=150).mark_line(size=1, color='darkred').encode(
+line = alt.Chart(df, width=1000, height=250).mark_line(size=1, color='darkred').encode(
     alt.X('Year:O',title = None),
     alt.Y('Value:Q',title = None, axis=None,),
     tooltip=['Location','Value', 'Unit', 'Year'],
     size=alt.value(3)
 ).transform_filter(
-    selection).transform_filter(alt.datum.Location == 'Red Lake, MN').transform_filter(alt.datum.Location != 'Minnesota').properties(width=600, height= 200)
+    selection).transform_filter(alt.datum.Location == 'Red Lake, MN').transform_filter(alt.datum.Location != 'Minnesota').properties(width=1100, height= 400)
 
 
-line2 = alt.Chart(df, width=500, height=150).mark_line(size=1).encode(
+line2 = alt.Chart(df, width=1000, height=250).mark_line(size=1).encode(
     alt.X('Year:O',title = None, axis=None),
     alt.Y('Value:Q',title = None, axis=None),
     tooltip=['Location','Value', 'Unit', 'Year'],
@@ -72,7 +72,7 @@ line2 = alt.Chart(df, width=500, height=150).mark_line(size=1).encode(
     
     size=alt.condition(highlight, alt.value(5), alt.value(1))
 ).transform_filter(selection # & (alt.datum.Location != 'Red Lake, MN')
-                  ).transform_filter(alt.datum.Location != 'Minnesota').properties(width=600, height= 200)
+                  ).transform_filter(alt.datum.Location != 'Minnesota').properties(width=1100, height= 400)
 
 
 text2 = line.mark_text(size = 30, dx = 10, align='left').encode(
@@ -84,24 +84,24 @@ text2 = line.mark_text(size = 30, dx = 10, align='left').encode(
     color = alt.value('darkred')
 ).transform_filter(
     selection).transform_filter((alt.datum.Location == 'Red Lake, MN')
-    & (alt.datum.Year == 2015)).properties(width=600, height= 200)
+    & (alt.datum.Year == 2015)).properties(width=1200, height= 400)
 
-rule=alt.Chart(df).mark_rule(color='#F0B863',size=25, strokeOpacity=0.1, fillOpacity=0.1, opacity=0.1).encode(
+rule=alt.Chart(df).mark_rule(color='#F0B863',size=50, strokeOpacity=0.1, fillOpacity=0.1, opacity=0.1).encode(
     alt.X('Year:T'),
 ).transform_filter(alt.datum.Year == int(year_select))
 
-line_axis = alt.Chart(df, width=900).mark_text(size = 10, dy = 100, align='center', color='#996C46').encode(
+line_axis = alt.Chart(df, width=900).mark_text(size = 15, dy = 190, align='center', color='#996C46').encode(
     alt.X('Year:T',title = None),
     text= alt.Text('Year:O')
 ).transform_filter(alt.datum.Location == 'Red Lake, MN')
 
-line_title = alt.Chart(df, width=900).mark_text(size = 15, dy = -120, align='center', color='#996C46').encode(
+line_title = alt.Chart(df, width=900).mark_text(size = 30, dy = -185, align='center', color='#996C46').encode(
     text= alt.Text('label2:O')
 ).transform_filter((alt.datum.Year == int(year_select))& alt.datum.Location == 'Red Lake, MN').transform_filter(selection
 ).transform_calculate(label2='"Minnesota State "+ datum.Category + " from 2000 to 2015"')
 
 #Second Bar Chart
-bar2 = alt.Chart(df, width=200, height=150).mark_bar(size=20).encode(
+bar2 = alt.Chart(df, width=400, height=400).mark_bar(size=45).encode(
   alt.X('Location:N',title = None,sort=alt.EncodingSortField(
         field='Value', order='ascending'
     ), axis=None),
@@ -116,7 +116,7 @@ bar2 = alt.Chart(df, width=200, height=150).mark_bar(size=20).encode(
     ) & (alt.datum.Year == int(year_select)) #Dropdown
 ).transform_filter(selection).properties(title='Red Lake Compared with Max, Min and State Average')
 
-bar_text = bar2.mark_text(size = 15, dy = -15, align='center').encode(
+bar_text = bar2.mark_text(size = 20, dy = -15, align='center').encode(
     alt.X('Location:N',title = None,sort=alt.EncodingSortField(
         field='Value', order='ascending'
     )
@@ -129,7 +129,7 @@ vis1= ((bar.add_selection(selection)+text1)&(
  (line2.add_selection(highlight, selection2)+rule+line+text2+points+line_axis+line_title)|(bar2+bar_text))).configure_view(strokeWidth=0).resolve_scale(color='independent', size='independent'
  ).configure_range(category={'scheme': 'browns'}).configure_title(
     color='#996C46',
-    fontSize=16
+    fontSize=20
 )
 
 
